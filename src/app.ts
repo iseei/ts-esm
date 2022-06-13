3c8da915e6f6db5f2f62731d20d1bd0479bb247d
han <qwertpiyi@gmail.com>
2022-06-14 00:31:31 +0800

    tsc --init

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// import { join, dirname } from 'path'
+// import { Low, JSONFile } from 'lowdb'
+// import { fileURLToPath } from 'url'
+
+console.log('app')
+
+function fn(n: number) {
+  if (n > 5) {
+    return true;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+function fn1(n: number) {
+  if (n > 5) {
+    return true;
+  } else {
+    return false;
+  }
+  return true;
+}
